2f2e63b4454373598fd6b56e50e48955204d3ccc
Saul Maldonado <samaldonado4@gmail.com>
2020-08-26 19:45:56 -0500

    Updates

---
+++ precache-manifest.d7e64e6c648f1b906be264810a858fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60e8118402b934e78a7c1e63c90a3bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c41c96723ea077f5c010",
+    "url": "./static/css/main.0872299c.chunk.css"
+  },
+  {
+    "revision": "aa400d6c350fa0752eed",
+    "url": "./static/js/2.d94b8667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d94b8667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41c96723ea077f5c010",
+    "url": "./static/js/main.0a7e8d54.chunk.js"
+  },
+  {
+    "revision": "564b5bb51dbfd2553aaf",
+    "url": "./static/js/runtime-main.3472bc24.js"
+  }
+]);
