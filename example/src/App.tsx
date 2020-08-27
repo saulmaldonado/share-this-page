c448e45bb1e92b7691390cf12b2501a2fb828f47
Saul Maldonado <samaldonado4@gmail.com>
2020-08-26 19:37:16 -0500

    Edit styles for component and landing page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,22 @@ import { ShareButton } from 'share-button';
 import 'share-button/dist/index.css';
 
 const App = () => {
-  return <ShareButton />;
+  return (
+    <>
+      <ShareButton />
+      <div className='text'>
+        <h1>Share This Page</h1>
+        <p>
+          Drop this zero-configuration React component in your application to
+          instantly generate social media sharing links to the current site you
+          are on
+        </p>
+        <code className='javascript'>
+          npm install @saulmaldonado/share-this-page
+        </code>
+      </div>
+    </>
+  );
 };
 
 export default App;
